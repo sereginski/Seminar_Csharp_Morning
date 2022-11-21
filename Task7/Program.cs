74cb5994deed52e6432e531b54e1b9e392394fb4
sereginski <relict1900@bk.ru>
2022-11-22 00:10:57 +0300

    task7

---
+++ Task7/Program.cs
@@ -0,0 +1,6 @@
+﻿// Task 7
+Console.Clear();
+Console.Write("Введите число: ");
+// Для того, чтобы найти последнюю цифру любого числа, необходимо найти остаток при делении на 10
+int n = Convert.ToInt32(Console.ReadLine()); // ввод чисел
+Console.Write(n % 10);
