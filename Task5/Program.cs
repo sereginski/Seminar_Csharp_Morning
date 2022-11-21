ecb9268214a9ecb3194b58b50c0d8489fc60ec1b
sereginski <relict1900@bk.ru>
2022-11-21 23:58:49 +0300

    task5

---
+++ Task5/Program.cs
@@ -0,0 +1,7 @@
+﻿// Task 5
+// task 5
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()); // ввод чисел
+for (int i = (-1) * n; i <= n; i++)
+Console.Write($"{i} ");
