13ca446e6a29e097a5dde2c8811c4ee5aedb5192
sereginski <relict1900@bk.ru>
2022-11-21 23:45:03 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,10 @@
+﻿// task 2
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (m * m == n)
+    Console.Write("yes");
+else    
+    Console.Write("no");
