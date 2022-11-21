8d7b7741c59fe57373efec98249b48beaf0e3584
sereginski <relict1900@bk.ru>
2022-11-21 23:47:25 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// task 3
+Console.Clear();
+Console.Write("Введите день недели: ");
+int n = Convert.ToInt32(Console.ReadLine()); // ввод чисел
+while (n < 1 || n > 7)
+{
+    Console.Write("Вы ошиблись!\nВведите день недели: ");
+    n = Convert.ToInt32(Console.ReadLine());    
+}
+if (n == 1)
+    Console.WriteLine("Понедельник");
+else if (n == 2)
+    Console.WriteLine("Вторник");
+else if (n == 3)
+    Console.WriteLine("Среда");
+else if (n == 4)
+    Console.WriteLine("Четверг");
+else if (n == 5)
+    Console.WriteLine("Пятница");
+else if (n == 6)
+    Console.WriteLine("Суббота");
+else
+    Console.WriteLine("Воскресенье");
