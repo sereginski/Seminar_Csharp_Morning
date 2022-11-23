a8d8b5add1489ac5a68d91e9469b58d95606d5c1
sereginski <relict1900@bk.ru>
2022-11-23 19:45:10 +0300

    task6

---
+++ Seminar1/homework/task6/Program.cs
@@ -0,0 +1,8 @@
+﻿// task 6 Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным 
+Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+    Console.Write("yes");
+else
+    Console.Write("no");
