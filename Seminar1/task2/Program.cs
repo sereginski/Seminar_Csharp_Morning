5bf0bd1fddf9bfecdaa375efd22379422521f5c5
sereginski <relict1900@bk.ru>
2022-11-23 19:04:39 +0300

    удалил папку task4

--- Seminar1/task2/Program.cs
+++ Seminar1/task2/Program.cs
@@ -1,10 +1,10 @@
-﻿// task 2
+﻿// task 4 Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 Console.Clear();
 Console.Write("Введите 1-ое число: ");
-int n = Convert.ToInt32(Console.ReadLine());
+int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите 2-ое число: ");
-int m = Convert.ToInt32(Console.ReadLine());
-if (m * m == n)
-    Console.Write("yes");
-else    
-    Console.Write("no");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+    Console.Write(a);
+else
+    Console.Write(b);
