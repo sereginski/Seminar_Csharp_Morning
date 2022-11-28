9dfeaee2be413dd81901d1e7d5ae68c80c5587d3
sereginski <relict1900@bk.ru>
2022-11-28 19:00:00 +0300

    task4

---
+++ Seminar2/task4/Program.cs
@@ -0,0 +1,11 @@
+﻿// task12
+Console.Clear();
+
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n % m == 0)
+Console.Write("yes");
+else
+Console.Write($"не кратно, остаток {n % m}");
