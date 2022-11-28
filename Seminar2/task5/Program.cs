6277cb40a58dbe2bfc9b88021d1a218533f1da85
sereginski <relict1900@bk.ru>
2022-11-28 19:01:40 +0300

    task5

---
+++ Seminar2/task5/Program.cs
@@ -0,0 +1,9 @@
+﻿//task14
+Console.Clear();
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if ((n % 7) + (n % 23) == 0)
+Console.Write("yes");
+else
+Console.Write("no");
