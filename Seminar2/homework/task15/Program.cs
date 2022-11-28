17597c3930876a98bbea5a97851b7be4398229d9
sereginski <relict1900@bk.ru>
2022-11-28 20:57:36 +0300

    hm_task15

---
+++ Seminar2/homework/task15/Program.cs
@@ -0,0 +1,8 @@
+﻿// Task15 Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день
+Console.Clear();
+Console.Write("Введите день недели: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (m == 6 || m == 7)
+    Console.Write("Выходной");
+else if ( m >0 && m < 6)
+    Console.Write("Иди работай");
