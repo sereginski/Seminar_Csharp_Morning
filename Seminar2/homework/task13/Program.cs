0acdeee97b91d5f9a75c134537726c11f06d3e01
sereginski <relict1900@bk.ru>
2022-11-28 20:47:31 +0300

    hm_task13

---
+++ Seminar2/homework/task13/Program.cs
@@ -0,0 +1,11 @@
+﻿// task13 Напишите программу, которая выводит третью цифру(c начала) заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+Console.Write("Введите число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (m<100) 
+    Console.Write("третьей цифры нет");
+else
+    while (m>=1000)
+        m/=10;
+        Console.Write(m%10);
+    
