c2e6dde3048c9dd47b39c63f8ca8384e6cf980a4
sereginski <relict1900@bk.ru>
2022-11-30 19:33:18 +0300

    task17

---
+++ Seminar3/task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// task17
+Console.Clear();
+Console.Write("Введите координату X: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+
+if (x > 0 && y > 0)
+    Console.WriteLine("I");
+else if (x < 0 && y > 0)
+    Console.WriteLine("II");
+else if (x < 0 && y < 0)
+    Console.WriteLine("III");
+else
+    Console.WriteLine("IV");
