1eaac3fd40df0f36e280e7fd43510f2481575df3
sereginski <relict1900@bk.ru>
2022-11-30 21:03:15 +0300

    task18

---
+++ Seminar3/task2(18)/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Ведите номер четверти: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n == 1)
+Console.WriteLine("x > 0 and y > 0");
+else if (n == 2)
+Console.WriteLine("x < 0 and y > 0");
+else if (n == 3)
+Console.WriteLine("x < 0 and y < 0");
+else
+Console.WriteLine("x > 0 and y < 0");
