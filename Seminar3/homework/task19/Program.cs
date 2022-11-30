2ecc95e6755b64fe8c5cf6d943c1456920ee71e6
sereginski <relict1900@bk.ru>
2022-11-30 21:46:20 +0300

    hm_task19

---
+++ Seminar3/homework/task19/Program.cs
@@ -0,0 +1,8 @@
+﻿// task19
+Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if ((m/10000 == m % 10)&&((m/1000)%10 == (m/10)%10))
+    Console.Write("yes");
+else
+    Console.Write("no");
