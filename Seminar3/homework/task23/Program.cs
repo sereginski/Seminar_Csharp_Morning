0304129f40839dd526acbca664091e4fd982e852
sereginski <relict1900@bk.ru>
2022-11-30 22:48:07 +0300

    hm_task23

---
+++ Seminar3/homework/task23/Program.cs
@@ -0,0 +1,7 @@
+﻿// task23
+Console.Clear(); 
+Console.Write("Введите число: "); 
+int n = Convert.ToInt32(Console.ReadLine()); 
+for (int i = 1; i <= n; i++)     
+Console.Write($"{i * i * i} ");
+
