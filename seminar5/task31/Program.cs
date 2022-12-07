b23745374db44714430d442b158e7d7fd5a975bf
sereginski <relict1900@bk.ru>
2022-12-07 22:07:33 +0300

    task31

---
+++ seminar5/task31/Program.cs
@@ -0,0 +1,31 @@
+﻿// task31
+
+void InputArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+array[i] = new Random().Next(-9, 10);
+}
+
+
+void ReleaseArray(int[] array)
+{
+int sumPositive = 0, sumNegative = 0;
+for (int i = 0; i < array.Length; i++)
+{
+if (array[i] > 0)
+sumPositive += array[i];
+else
+sumNegative += array[i];
+}
+Console.WriteLine($"Сумма положительных чисел в массиве {sumPositive}");
+Console.WriteLine($"Сумма отрицательных чисел в массиве {sumNegative}");
+}
+
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+ReleaseArray(array);
