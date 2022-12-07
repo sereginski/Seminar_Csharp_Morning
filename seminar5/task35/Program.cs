b2952cd1f51b956eba2c89080ac5133060f816af
sereginski <relict1900@bk.ru>
2022-12-07 23:36:25 +0300

    task37

--- seminar5/task35/Program.cs
+++ seminar5/task35/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// task 35
+void InputArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+array[i] = new Random().Next(-100, 101);
+}
+
+
+int ReleaseArray(int[] array)
+{
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+if (array[i] >= 10 && array[i] <= 99)
+count++;
+}
+return count;
+}
+
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[123];
+InputArray(array);
+Console.WriteLine($"Исходный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Результат: {ReleaseArray(array)}");
