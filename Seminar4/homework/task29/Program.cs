12381269dbfdfbd827d03781d5ca3bf024e0a3bd
sereginski <relict1900@bk.ru>
2022-12-07 17:55:11 +0300

    hm_task29

---
+++ Seminar4/homework/task29/Program.cs
@@ -0,0 +1,9 @@
+﻿// task29
+Console.Clear();
+int [] array = new int [8];
+for (int i = 0; i < 8; i++)
+{
+    Console.Write("Введите число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"[{string.Join(", ", array)}]");
