590965aeb651634659ac0bce0c89e3cd11eb60d3
sereginski <relict1900@bk.ru>
2022-12-07 11:58:23 +0300

    Seminar4_task30

---
+++ Seminar4/task30/Program.cs
@@ -0,0 +1,6 @@
+﻿// task30
+Console.Clear();
+int[] array = new int[8];
+for (int i = 0; i < 8; i++)
+    array[i] = new Random().Next(0, 2);
+Console.WriteLine($"Результат: [{string.Join(", ", array)}]");
