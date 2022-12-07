c935d68296f663c973db2b92e0563876877551f9
sereginski <relict1900@bk.ru>
2022-12-07 11:52:15 +0300

    Seminar4_task28

---
+++ Seminar4/task28/Program.cs
@@ -0,0 +1,8 @@
+﻿// task28
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), result = 1;
+for (int i = 1; i <= n; i++)
+    result *= i; // result = result *i
+    Console.WriteLine($"Результат: {result}");
+    
