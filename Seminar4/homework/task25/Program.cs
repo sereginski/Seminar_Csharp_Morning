7f5fd62cca6599e2702dd4a7ca32c5c246af8a52
sereginski <relict1900@bk.ru>
2022-12-07 12:10:52 +0300

    hm_task25

---
+++ Seminar4/homework/task25/Program.cs
@@ -0,0 +1,10 @@
+﻿// task25
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int result=1;
+for (int i=1; i<=m;i++)
+result *= n;
+Console.Write(result);
