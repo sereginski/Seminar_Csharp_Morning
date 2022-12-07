fdd0ada4f7dfdcef9865e94b2b3568fad118dc23
sereginski <relict1900@bk.ru>
2022-12-07 17:51:09 +0300

    hm_task27

---
+++ Seminar4/homework/task27/Program.cs
@@ -0,0 +1,12 @@
+﻿// task27
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while( n > 0)
+{
+int number = n % 10;
+n = n / 10;
+sum = sum + number;
+}
+Console.WriteLine($"Сумма цифр = {sum}");
