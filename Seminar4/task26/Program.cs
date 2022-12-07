6de41345df06939f89e67ba790961da409a2c8d1
sereginski <relict1900@bk.ru>
2022-12-07 11:40:00 +0300

    Seminar4_task26

---
+++ Seminar4/task26/Program.cs
@@ -0,0 +1,11 @@
+﻿// task26
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int size = 1;
+while (n / 10 !=0)
+{
+    n /= 10;
+    size++;
+}
+Console.WriteLine(size);
