19bea2921b97a277222cf79e3ce57bdb904a3f8f
sereginski <relict1900@bk.ru>
2022-12-12 15:56:07 +0300

    task36

---
+++ seminar5/homework/hm_task36/Program.cs
@@ -0,0 +1,30 @@
+﻿// task36
+int[] GetArrayRandomNumbers(int n)
+{
+   int[] array = new int[n];
+   for (int i = 0; i < array.Length; i++)
+   {
+       array[i] = new Random().Next(1,1000);
+   }
+   return array;
+}
+int GetSumNumbersEvenPosition(int[] array)
+{
+    int sum = 0;
+  for(int i = 0; i < array.Length; i++)
+  {
+    if(i % 2 == 0) 
+    {
+      sum = sum + array[i];
+    }
+  }
+  return sum;
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] mas = GetArrayRandomNumbers(n);
+Console.WriteLine($"Исходный массив: [{string.Join(", ", mas)}]");
+int rezult = GetSumNumbersEvenPosition(mas);
+Console.WriteLine($"Сумма чисел стоящих на четных позициях = {rezult}");
