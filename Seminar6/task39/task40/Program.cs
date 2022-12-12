1ca2d35731292e5b813cc73d539c461af0641351
sereginski <relict1900@bk.ru>
2022-12-12 18:44:32 +0300

    task40

---
+++ Seminar6/task39/task40/Program.cs
@@ -0,0 +1,12 @@
+﻿//  task40
+Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a < b + c && b < a + c && c < a + b)
+Console.WriteLine("Может");
+else
+Console.WriteLine("Не может");
