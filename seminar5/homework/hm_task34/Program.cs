a32700e0be9954b240e28c498c7237b82b58ecdd
sereginski <relict1900@bk.ru>
2022-12-12 15:53:47 +0300

    task34

---
+++ seminar5/homework/hm_task34/Program.cs
@@ -0,0 +1,23 @@
+﻿// task34
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000);
+}
+
+int numberEven (int[] array)
+{
+    int result=0;
+    for (int i = 0; i < array.Length; i++)
+    if (array[i]%2==0)
+        result++;
+    return result;
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Исходный массив: [{string.Join(", ", array)}]");
+Console.WriteLine(numberEven(array));
