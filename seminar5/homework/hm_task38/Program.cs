3e59d4c13fc9b93e1997a1660cbee5216a919c45
sereginski <relict1900@bk.ru>
2022-12-12 16:00:31 +0300

    task38

---
+++ seminar5/homework/hm_task38/Program.cs
@@ -0,0 +1,28 @@
+﻿// task38
+void InputArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-99, 100);
+}
+
+double number (double[] array)
+{
+    double max = array[0];
+    double min= array[0];
+    for (int i = 0; i < array.Length; i++)
+    if (array[i]>max)
+        max=array[i];
+    else if (array[i]<min)
+        min=array[i];
+    double result = max - min;
+
+    return result;
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+InputArray(array);
+Console.WriteLine($"Исходный массив: [{string.Join(", ", array)}]");
+Console.WriteLine(number(array));
