b8396243862b2b799d020113cbe8f5ab40c6fd0b
sereginski <relict1900@bk.ru>
2022-12-12 18:47:28 +0300

    task42

---
+++ Seminar6/task42/Program.cs
@@ -0,0 +1,11 @@
+﻿// task42
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string result = String.Empty;
+while (n > 0)
+{
+result = Convert.ToString(n % 2) + result;
+n /= 2;
+}
+Console.WriteLine(result);
