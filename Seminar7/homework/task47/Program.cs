536d1a097b79a9e229fdbda955ff67d691044f7e
sereginski <relict1900@bk.ru>
2022-12-15 17:43:09 +0300

    task47

---
+++ Seminar7/homework/task47/Program.cs
@@ -0,0 +1,19 @@
+﻿// task47
+void InputMatrix(double [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round( (new Random().NextDouble() * (10 + 10) -10), 2); 
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введи размер матрицы: ");
+string[] numbers = Console.ReadLine().Split(" ");
+double [,] matrix = new double[int.Parse(numbers[0]), int.Parse(numbers[1])];
+InputMatrix(matrix);
