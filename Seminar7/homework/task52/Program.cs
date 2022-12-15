25bd8a510580e19b788c108723d008ed1b23e5cf
sereginski <relict1900@bk.ru>
2022-12-15 17:50:21 +0300

    task52

---
+++ Seminar7/homework/task52/Program.cs
@@ -0,0 +1,35 @@
+﻿//task52
+Console.Write("введите количество строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите количество столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+
+void InputmArray(int[,] matrix)
+ {
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+       for(int j = 0; j < matrix.GetLength(1); j++)
+       {
+          matrix[i, j] = new Random().Next(0, 10);
+           Console.Write($"{matrix[i, j]} \t");
+       }
+        Console.WriteLine();
+    }
+ } 
+ void arithmeticMeanElements(int[,] matrix)
+ {
+    for(int j = 0; j < matrix.GetLength(1); j++)
+    {
+       int avarage = 0;
+       for(int i = 0; i < matrix.GetLength(0); i++)
+     {
+         avarage = (avarage + matrix[i, j]);
+     }
+    avarage = avarage / n;
+    Console.WriteLine($"средне арифметическое элементов в столбце = {avarage} ");
+    } 
+ }
+Console.Clear();
+InputmArray(matrix);
+arithmeticMeanElements(matrix);
