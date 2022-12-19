3f285271951bd6fa48bc5b4d782332dcd3219c7b
sereginski <relict1900@bk.ru>
2022-12-19 19:23:02 +0300

    task62

---
+++ Seminar8/homework/task62/Program.cs
@@ -0,0 +1,68 @@
+﻿// task62
+void OutputMatrix(int[,] matrix)
+
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.Clear();
+int[,] matrix = new int[4, 4];
+int j = 0;
+int i = 0;
+int number = 1;
+int count = 0;
+
+int kj = matrix.GetLength(0) - 1; //3
+int ki = matrix.GetLength(1) - 1; //3
+
+while (count < matrix.GetLength(0) * matrix.GetLength(1))
+{
+    while (kj - j >= 0 && count < matrix.GetLength(0) * matrix.GetLength(1))
+    {
+        matrix[i, j] = number++;
+        count++;
+        j++;
+    }
+    j--;
+    i++;
+    kj--;
+
+
+    while (ki - i >= 0 && count < matrix.GetLength(0) * matrix.GetLength(1))
+    {
+        matrix[i, j] = number++;
+        count++;
+        i++;
+    }
+    i--;
+    j--;
+    ki--;
+
+    while (kj - j <= kj && count < matrix.GetLength(0) * matrix.GetLength(1))
+    {
+        matrix[i, j] = number++;
+        count++;
+        j--;
+    }
+    j++;
+    i--;
+
+
+    while (ki + i > ki && count < matrix.GetLength(0) * matrix.GetLength(1))
+    {
+        matrix[i, j] = number++;
+        count++;
+        i--;
+    }
+    i++;
+    j++;
+}
+OutputMatrix(matrix);
