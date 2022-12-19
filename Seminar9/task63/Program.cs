762453bf02618067ca9b3713f2ed6262e420552b
sereginski <relict1900@bk.ru>
2022-12-19 19:32:07 +0300

    task63

---
+++ Seminar9/task63/Program.cs
@@ -0,0 +1,13 @@
+﻿// task63
+string rec(int n)
+{
+if (n == 1)
+return "1 ";
+return rec(n - 1) + $"{n} ";
+}
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(n));
