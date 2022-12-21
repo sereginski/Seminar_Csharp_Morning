31a4f0ffb4954c7dc202481ee9ff9f78bd43c070
sereginski <relict1900@bk.ru>
2022-12-21 13:07:38 +0300

    task68

---
+++ Seminar9/homework/task68/Program.cs
@@ -0,0 +1,19 @@
+﻿//task68
+int rec(int m, int n)
+{
+    if (m == 0)
+        return (n + 1);
+    else if (m > 0 && n == 0)
+        return rec(m - 1, 1);
+    return rec(m - 1, rec(m, n - 1));
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (m < 0 || n < 0)
+    Console.Write("Необходимо ввести неотрицательные числа");
+else
+    Console.WriteLine(rec(m, n));
