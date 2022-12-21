bae63189f96eff979e8418bfe3f7595eaa1c42bf
sereginski <relict1900@bk.ru>
2022-12-21 13:06:10 +0300

    task66

---
+++ Seminar9/homework/task66/Program.cs
@@ -0,0 +1,14 @@
+﻿// task66
+int rec(int m, int n)
+{
+    if (m > n)
+        return 0;
+    return rec(m+1, n) + m;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(m, n));
